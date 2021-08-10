a5419d0aaf59f2aa74a6381b89f48e07cc16ffd0
craigparra <craig.parravicini@aurizon.com.au>
2021-08-10 16:51:39 +1000

    1.0.12 / 2021-08-10 ==================
    * Fix Application export - @craigparra

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Boot = require('./Boot');
 const Application = require('./Application');
 
-// module.exports.Application = Application;
+module.exports.Application = Application;
 module.exports.Boot = Boot;
 module.exports.boot = Boot.boot;
 module.exports.root = Boot.root;
