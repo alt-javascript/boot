090b48ca8beb546a8a1e056507ab2916b2ffa2c1
craigparra <craig.parravicini@aurizon.com.au>
2021-08-16 15:19:28 +1000

    1.0.14 / 2021-08-16 ==================
    * Dependencies, config exported - @craigparra

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const Boot = require('./Boot');
 const Application = require('./Application');
+const Boot = require('./Boot');
+const { config } = require ('@alt-javascript/config');
 
 module.exports.Application = Application;
 module.exports.Boot = Boot;
 module.exports.boot = Boot.boot;
 module.exports.root = Boot.root;
 module.exports.test = Boot.test;
+module.exports.config = config;
