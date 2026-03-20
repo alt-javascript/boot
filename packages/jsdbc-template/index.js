bc44660c57befe142b93e917e55b9c580b9c6494
craigparra <craigparra@gmail.com>
2026-03-20 11:02:58 +1000

    feat: add @alt-javascript/jsdbc-template package
    Spring-inspired JsdbcTemplate and NamedParameterJsdbcTemplate
    moved from staging into the altjs monorepo. References published
    @alt-javascript/jsdbc-core and jsdbc-sqljs packages from npm.
    
    Includes TransactionTemplate for single-connection tx callbacks.
    24 tests passing. 390 total across monorepo.

---
+++ packages/jsdbc-template/index.js
@@ -0,0 +1,2 @@
+export { default as JsdbcTemplate, TransactionTemplate } from './JsdbcTemplate.js';
+export { default as NamedParameterJsdbcTemplate, parseNamedParams } from './NamedParameterJsdbcTemplate.js';
